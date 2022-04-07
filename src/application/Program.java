a6b510f4ff76d7f61389f1497ccd0da16dd97267
Elves Aguiar <elvesmhs@hotmail.com>
2022-04-07 15:13:10 -0300

    Versao final

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,41 @@
 package application;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.Scanner;
+
+import model.entities.Reservation;
+import model.exceptions.DomainException;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 
+		try {
+			System.out.print("Room number: ");
+			int number = sc.nextInt();
+			sc.nextLine();
+			System.out.print("Check - in date (dd/MM/yyyy): ");
+			Date checkIn = sdf.parse(sc.nextLine());
+
+			System.out.print("Check - in date (dd/MM/yyyy): ");
+			Date checkOut = sdf.parse(sc.nextLine());
+
+			Reservation reservation = new Reservation(number, checkIn, checkOut);
+			reservation.updateDates(checkIn, checkOut);
+			System.out.println("Reservation: " + reservation);
+		} catch (ParseException e) {
+			System.out.println("Invalid date format");
+		} catch (DomainException e) {
+			System.out.println(e.getMessage());
+		}
+
+		sc.close();
 	}
 
 }
--- src/model/entities/Reservation.java
+++ src/model/entities/Reservation.java
@@ -1,14 +1,73 @@
 package model.entities;
 
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
+
+import model.exceptions.DomainException;
 
 public class Reservation {
 	private Integer roomNumber;
 	private Date checkIn;
 	private Date checkOut;
 	private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+	public static final Calendar cal = new Calendar() {
+
+		/**
+		 * 
+		 */
+		private static final long serialVersionUID = 2L;
+
+		@Override
+		public void roll(int field, boolean up) {
+			// TODO Auto-generated method stub
+
+		}
+
+		@Override
+		public int getMinimum(int field) {
+			// TODO Auto-generated method stub
+			return 0;
+		}
+
+		@Override
+		public int getMaximum(int field) {
+			// TODO Auto-generated method stub
+			return 0;
+		}
+
+		@Override
+		public int getLeastMaximum(int field) {
+			// TODO Auto-generated method stub
+			return 0;
+		}
+
+		@Override
+		public int getGreatestMinimum(int field) {
+			// TODO Auto-generated method stub
+			return 0;
+		}
+
+		@Override
+		protected void computeTime() {
+			// TODO Auto-generated method stub
+
+		}
+
+		@Override
+		protected void computeFields() {
+			// TODO Auto-generated method stub
+
+		}
+
+		@Override
+		public void add(int field, int amount) {
+			// TODO Auto-generated method stub
+
+		}
+	};
 
 	public Reservation() {
 
@@ -42,22 +101,22 @@ public class Reservation {
 		return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
 	}
 
-	public void updateDates(Date checkin, Date checkout) {
-		this.checkIn = checkin;
-		this.checkOut = checkout;
+	public void updateDates(Date checkIn, Date checkOut) {
+		Date now = Calendar.getInstance().getTime();
+		if (checkIn.before(now) || checkOut.before(now)) {
+			throw new DomainException("Reservation dates for update must be future dates");
+		}
+		if (!checkOut.after(checkIn)) {
+			throw new DomainException("Check-out date must be after check-in date");
+		}
+		this.checkIn = checkIn;
+		this.checkOut = checkOut;
 	}
 
 	@Override
 	public String toString() {
-		return "Room "
-				+roomNumber
-				+", check-in: "
-				+sdf.format(checkIn)
-				+", check-out: "
-				+sdf.format(checkOut)
-				+", "
-				+duration()
-				+" nigths";
-	}			
+		return "Room " + roomNumber + ", check-in: " + sdf.format(checkIn) + ", check-out: " + sdf.format(checkOut)
+				+ ", " + duration() + " nigths";
+	}
 
 }
