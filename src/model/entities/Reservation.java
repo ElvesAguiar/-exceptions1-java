085d36694b9fbff29bf4598c77fe822ad423f428
Elves Aguiar <elvesmhs@hotmail.com>
2022-04-07 14:24:37 -0300

    Lapidando reservation

--- src/model/entities/Reservation.java
+++ src/model/entities/Reservation.java
@@ -1,14 +1,23 @@
 package model.entities;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
 public class Reservation {
 	private Integer roomNumber;
-	private Date checkin;
-	private Date checkout;
+	private Date checkIn;
+	private Date checkOut;
+	private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 
 	public Reservation() {
 
+	}
+
+	public Reservation(Integer roomNumber, Date checkin, Date checkout) {
+		this.roomNumber = roomNumber;
+		this.checkIn = checkin;
+		this.checkOut = checkout;
 	}
 
 	public Integer getRoomNumber() {
@@ -20,39 +29,35 @@ public class Reservation {
 	}
 
 	public Date getCheckin() {
-		return checkin;
-	}
-
-	public void setCheckin(Date checkin) {
-		this.checkin = checkin;
+		return checkIn;
 	}
 
 	public Date getCheckout() {
-		return checkout;
+		return checkOut;
 	}
 
-	public void setCheckout(Date checkout) {
-		this.checkout = checkout;
-	}
+	public long duration() {
+		long diff = checkIn.getTime() - checkOut.getTime();
 
-	public Reservation(Integer roomNumber, Date checkin, Date checkout) {
-		this.roomNumber = roomNumber;
-		this.checkin = checkin;
-		this.checkout = checkout;
-	}
-
-	public Integer duration() {
-		return null;
+		return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
 	}
 
 	public void updateDates(Date checkin, Date checkout) {
+		this.checkIn = checkin;
+		this.checkOut = checkout;
 	}
 
 	@Override
 	public String toString() {
-		return "Reservation [roomNumber=" + roomNumber + ", checkin=" + checkin + ", checkout=" + checkout + "]";
-	}
-	
-	
+		return "Room "
+				+roomNumber
+				+", check-in: "
+				+sdf.format(checkIn)
+				+", check-out: "
+				+sdf.format(checkOut)
+				+", "
+				+duration()
+				+" nigths";
+	}			
 
 }
